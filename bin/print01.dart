4fcd12d3f2f19800bb598b783d15766c1140ee0d
nnn <maxkambekxolbekov515@gmail.com>
2022-07-16 09:37:07 -0700

    print

---
+++ bin/print01.dart
@@ -0,0 +1,19 @@
+void main(){
+    print (' _________________');
+    print ('|     ^ ___ *     |');
+    print ('| _______________ |');
+    print ('||               ||');
+    print ('||     12:00     ||');
+    print ('||saturday,16july||');
+    print ('||               ||');
+    print ('||               ||');
+    print ('||     __o       ||');
+    print ('||   _ \\<_       ||');
+    print ('||  (_)/(_)      ||');
+    print ('||               ||');
+    print ('||  press  home  ||');
+    print ('||   to unlock   ||');
+    print ('||_______________||');
+    print ('|      [   ]      |');
+    print ('|_________________|');
+}
